fbced616201f39be8a5d597112784c4325933594
Weazzzzzzey <heronightmares@gmail.com>
2020-11-14 15:34:21 +0200

    Pridedami Pasiruosimo duomenys

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
 export default function App() {
   return (
